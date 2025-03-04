8e62b4aa275775d43cf892483fba7914ac0abb4c
Angelika Brown <angelikpbrown@gmail.com>
2025-03-03 20:24:04 -0500

    created a chair

--- object1/src/main/java/object1/Chair.java
+++ object1/src/main/java/object1/Chair.java
@@ -1,4 +1,39 @@
 package object1;
 
 public class Chair {
+    private String material;
+    private String color;
+    private int legs;
+
+    //constructor
+    public Chair(String material, String color, int legs) {
+        this.material = material;
+        this.color = color;
+        this.legs = legs;
+    }
+
+    //getters and setters
+    public String getMaterial(){
+        return material;
+    }
+
+    public void setMaterial(){
+        this.material = material;
+    }
+
+    public String setColor() {
+        return color;
+    }
+
+    public void getColor() {
+        this.color = color;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
 }
